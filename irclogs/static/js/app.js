ff195c6d2118e683635393cfa876164fcd7cbdfd
Dean Silfen <dean.silfen@gmail.com>
2014-03-25 17:54:59 -0400

    change message number to 25 per page, attempt at pagination almost complete

--- irclogs/static/js/app.js
+++ irclogs/static/js/app.js
@@ -11,6 +11,23 @@ app.config(['$routeProvider', function($routeProvider) {
 
 app.controller('MessageListCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
     $scope.searchQuery = $location.search().q || '';
+    $scope.page_num = 1;
+    $scope.next_page = function() {
+        if ($scope.page_num == $scope.total_pages)
+            return undefined;
+        else {
+            $scope.page_num++;
+            $scope.fetch();
+        }
+    };
+    $scope.last_page = function() {
+        if ($scope.page_num == 1)
+            return undefined;
+        else {
+            $scope.page_num--;
+            $scope.fetch();
+        }
+    };
     $scope.fetch = function() {
         if($scope.searchQuery.trim().length) {
             $location.search('q', $scope.searchQuery.trim());
@@ -24,9 +41,11 @@ app.controller('MessageListCtrl', ['$scope', '$http', '$location', function($sco
                     op: 'like',
                     val: '%' + $scope.searchQuery + '%',
                 }]},
+            page: $scope.page_num,
             },
         }).success(function(data) {
             $scope.messages = data.objects;
+            $scope.total_pages = data.total_pages;
         });
     };
 }]);
